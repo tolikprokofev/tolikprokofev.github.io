962612ca85a6e85579afd07a5c042936bd20cb8e
tolikprokofev <kitkat.tolik@mail.ru>
2017-05-09 18:24:17 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -8,6 +8,18 @@ $(function() {
 		e.preventDefault();
 		menu.slideToggle();
 	});
+        $(window).resize(function(){
+
+    	var w = $(window).width();
+
+    	if(w > 960 && menu.is(':hidden')) {
+
+        menu.removeAttr('style');
+
+    }
+
+});
+
 });
 
 
